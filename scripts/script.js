ef979fc33c7aee901ed4457e4c102dba882bbf1e
badanaut <49814350+badanaut@users.noreply.github.com>
2023-01-05 20:41:45 +0200

    Add User Interface
    User interface added, functions updated.
    Display the winner at the end of the game
    
    [Ticket: xxx]

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,30 @@
+let playerScore=0;
+let computerScore=0; 
+
+const player_card = document.querySelector('#player-card')
+const computer_card = document.querySelector('#computer-card')
+const round_result = document.querySelector('#round-result')
+
+
+const player_score = document.createElement("p");
+const computer_score = document.createElement("p");
+//set initial score
+player_score.textContent = "0";
+computer_score.textContent = "0";
+
+player_card.appendChild(player_score);
+computer_card.appendChild(computer_score);
+
+// const btn = document.querySelectorAll(".btn")
+// let playerSelection;
+// btn.forEach((button => {
+//     button.addEventListener('click',() => {
+//         playerSelection = button.id;
+//     });
+// }));
+
+
+
 function getComputerChoice(){
 
     //1. define the choices
@@ -8,83 +35,94 @@ function getComputerChoice(){
     return computerChoice
 }
 
-function getPlayerSelection(){
-    //ask player for selection
-    let playerSelection = prompt("Your choice: ");
-    //convert to lower case to avoid typo errors
-    playerSelection = playerSelection.toLocaleLowerCase();
-    //check if the choice is valid
-    if (!["rock", "paper", "scissors"].includes(playerSelection)){
-        playerSelection = prompt("Invalid selection, please select rock, paper or scissors. Your choice: ");
-    }
+function playRound(playerSelection) {
+    
+    computerSelection = getComputerChoice()
 
-    return playerSelection
+    if (playerSelection===computerSelection){
+        round_result.textContent = "Draw. Both players selected " + playerSelection;
+        return
+    }
+    else if(playerSelection==="rock" && computerSelection==="paper"){
+        computerScore++;
+        round_result.textContent = "You Lose! Rock is beaten by Paper";
+        return
+    }
+    else if(playerSelection==="rock" && computerSelection==="scissors"){
+        playerScore++;
+        round_result.textContent = "You Win! Rock beats Scissors";
+        return
+    }
+    else if(playerSelection==="paper" && computerSelection==="rock"){
+        playerScore++;
+        round_result.textContent = "You Win! Paper beats Rock";
+        return
+    }
+    else if(playerSelection==="paper" && computerSelection==="scissors"){
+        computerScore++;
+        round_result.textContent = "You Lose! Paper is beaten by Scissors";
+        return
+    }
+    else if(playerSelection==="scissors" && computerSelection==="rock"){
+        computerScore++;
+        round_result.textContent = "You Lose! Scissors are beaten by Rock";
+        return
+    }
+    else if(playerSelection==="scissors" && computerSelection==="paper"){
+        playerScore++;
+        round_result.textContent = "You Win! Scissors beats Paper";
+        return
+    }
+    
+ }
+
+function scoreReset(){
+    playerScore=0;
+    computerScore=0; 
+    player_score.textContent = "0";
+    computer_score.textContent = "0";
+    round_result.textContent = ""
+}
+// Get the modal
+let modal = document.getElementById("myModal");
+let modal_btn = document.getElementById("modal-btn");
+let modal_message = document.getElementById("modal-message");
+
+function displayWinner(winner){
+    modal_message.textContent = winner;
+    modal.style.display = "flex";
+    modal.style.flex = center;
+}
+
+function game(playerSelection){
+    if(playerScore<5 && computerScore<5){
+
+        playRound(playerSelection);
+        
+        player_score.textContent = playerScore;
+        computer_score.textContent = computerScore;
+     }
+     if (playerScore==5){
+        displayWinner("You win!");
+     }
+     else if(computerScore==5){
+        displayWinner("Computer win!");
+     }
+     
 }
 
 
-function playRound(playerSelection = getPlayerSelection(), computerSelection = getComputerChoice()) {
-
-    if (playerSelection===computerSelection){
-        console.log("Draw. Both players selected " + playerSelection);
-        return "draw";
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      scoreReset();
     }
-    else if(playerSelection==="rock" && computerSelection==="paper"){
-        console.log("You Lose! Paper beats Rock");
-        return "computer";
-    }
-    else if(playerSelection==="rock" && computerSelection==="scissors"){
-        console.log("You Win! Rock beats Scissors");
-        return "player";
-    }
-    else if(playerSelection==="paper" && computerSelection==="rock"){
-        console.log("You Win! Paper beats Rock");
-        return "player"
-    }
-    else if(playerSelection==="paper" && computerSelection==="scissors"){
-        console.log("You Lose! Scissors beats Paper");
-        return "computer"
-    }
-    else if(playerSelection==="scissors" && computerSelection==="rock"){
-        console.log("You Lose! Rock beats Scissors");
-        return "computer"
-    }
-    else if(playerSelection==="scissors" && computerSelection==="paper"){
-        console.log("You Win! Scissors beats Paper");
-        return "player"
-    }
-    
+  }
+// When the user clicks on button, close the modal
+modal_btn.onclick = function() {
+    modal.style.display = "none";
+    scoreReset();
   }
 
-  function game(){
-    let roundNumber=1;
-    let playerScore=0;
-    let computerScore=0; 
-
-    while(playerScore<5 && computerScore<5){
-        // round number
-        
-        console.log("Round number " + roundNumber +":");
-        let roundResult = playRound();
-        
-        if (roundResult==="draw"){
-            roundNumber++;
-            console.log("Score: Player " + playerScore + " : " + computerScore + " Computer")
-        }
-        else if(roundResult==="player"){
-            roundNumber++;
-            playerScore++;
-            console.log("Score: Player " + playerScore + " : " + computerScore + " Computer")
-        }
-        else if(roundResult==="computer"){
-            roundNumber++;
-            computerScore++;
-            console.log("Score: Player " + playerScore + " : " + computerScore + " Computer")
-        }
-     }
-     if (playerScore>computerScore){
-        console.log("Player wins!")
-     }
-     else{
-        console.log("Computer wins!")
-     }
-  }
+  
